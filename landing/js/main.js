8556619480a2c79549a456d4538c031cce37842e
Henrique Dias <hacdias@gmail.com>
2016-09-02 16:22:55 +0100

    update

--- landing/js/main.js
+++ landing/js/main.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let input = document.querySelector("#email input");
+    let span = document.querySelector("#email span");
+    let email = document.querySelector("#email");
 
+    span.addEventListener("click", () => {
+        email.classList.add("active");
+    });
+
+    input.addEventListener("blur", () => {
+        if (input.value == "") {
+            email.classList.remove("active");
+        }
+    });
+});
