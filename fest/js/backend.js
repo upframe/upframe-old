58d39a7e578aa425411654d676451a297e1436b9
Henrique Dias <hacdias@gmail.com>
2016-09-10 22:55:02 +0100

    halloween candies

--- fest/js/backend.js
+++ fest/js/backend.js
@@ -1,45 +1,162 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // get all edit buttons
-    var btns = document.getElementsByClassName("btnEdit");
-    Array.from(btns).forEach(editHandler);
+'use strict';
 
-    document.getElementById("single-form").addEventListener('submit', submitHandler);
+var singleForm,
+    monthNames = [
+        "Jan", "Feb", "Mar",
+        "Apr", "May", "Jun", "Jul",
+        "Aug", "Sep", "Oct",
+        "Nov", "Dec"
+    ];
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Initializes the Single Form variable
+    singleForm = document.getElementById("single-form");
+
+    if (singleForm) {
+        // Add an event listener to the Single Form
+        singleForm.addEventListener('submit', submitHandler);
+
+        // Get all the edit buttons and initialize them
+        var btns = document.getElementsByClassName("btnEdit");
+        Array.from(btns).forEach(editHandler);
+    }
 });
 
 function editHandler(btn) {
     btn.addEventListener("click", function(e) {
         e.preventDefault();
-        // get row data
-        document.getElementById("single-form").className = "fadeIn";
-        var row = btn.parentElement.parentElement;
-        for (x = 0; x < row.childElementCount - 1; x++) {
-            let data = row.children[x].dataset.name;
-            let input = document.querySelector("input[name=" + data + "]");
-            let barID = document.querySelector("#barID");
-            if (data == "ID") {
-                barID.innerHTML = row.children[x].innerHTML;
-            }
-
-            switch (input.type) {
-                case "datetime-local":
-                    input.value = new Date(row.children[x].innerHTML).toISOString().substr(0, 16);
-                    break;
-                case "checkbox":
-                    input.checked = row.children[x].children[0].checked;
-                    break;
-                default:
-                    input.value = row.children[x].innerHTML;
-            }
-        }
+        // fades in the form and copies the row information to the form
+        singleForm.className = "fadeIn";
+        copyRowToForm(btn.parentElement.parentElement);
     });
 }
 
 function submitHandler(event) {
     event.preventDefault();
 
-    var object = new Object();
+    let data = copyFormToObject(this);
+    let method = (data.ID == 0) ? 'POST' : 'PUT';
+    let link = this.dataset.link;
+
+    if (data.ID != 0) {
+        link += "/" + data.ID;
+    }
+
+    let request = new XMLHttpRequest();
+    request.open(method, link);
+    request.send(JSON.stringify(data));
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                singleForm.className = "fadeOut";
+
+                if (method == "PUT") {
+                    copyFormToRow(document.querySelector('tr[data-id="' + data.ID + '"]'));
+                } else {
+                    window.location.pathname = "/admin/" + window.location.pathname.split("/")[2] + "/" + request.responseText;
+                }
+            } else {
+                formError("Something went wrong.", "error")
+            }
+        }
+    }
+}
+
+function copyFormToRow(row) {
+    let form = singleForm;
+
+    if (typeof form == 'undefined') {
+        return;
+    }
+
+    let div = form.querySelector('div');
+
+    if (typeof div == 'undefined') {
+        return;
+    }
+
+    for (var i = 0; i < div.childElementCount; i++) {
+        let name = div.children[i].name;
+
+        if (typeof name == 'undefined' || name == null || name == "") {
+            continue;
+        }
+
+        let space = row.querySelector('td[data-name="' + name + '"]');
+
+        if (typeof space == 'undefined' || space == null) {
+            continue;
+        }
+
+        switch (div.children[i].type) {
+            case "datetime-local":
+                space.innerHTML = getPrettyDate(new Date(div.children[i].value));
+                break;
+            case "checkbox":
+                space.querySelector('input[type="checkbox"]').checked = div.children[i].checked;
+                break;
+            default:
+                space.innerHTML = div.children[i].value;
+        }
+    }
+}
+
+// getPrettyDate puts the date in a 22 Oct 99 08:48 UTC pretty format
+function getPrettyDate(date) {
+    let pretty;
+
+    let normalize = function(number) {
+        if (number.length == 1) {
+            return "0" + number;
+        }
+
+        return number;
+    }
+
+    pretty = normalize(date.getUTCDate().toString()) + " "
+    pretty += monthNames[date.getUTCMonth()] + " "
+    pretty += date.getUTCFullYear().toString().substr(2, 2) + " "
+    pretty += normalize(date.getUTCHours().toString()) + ":"
+    pretty += normalize(date.getUTCMinutes().toString()) + " UTC"
+
+    return pretty;
+}
+
+// Copies the information from a row to the editor form
+function copyRowToForm(row) {
+    let form = singleForm;
+
+    if (typeof form == 'undefined') {
+        return;
+    }
+
+    for (var x = 0; x < row.childElementCount - 1; x++) {
+        let data = row.children[x].dataset.name,
+            input = form.querySelector("input[name=" + data + "]"),
+            barID = form.querySelector("#barID");
+
+        if (data == "ID") {
+            barID.innerHTML = row.children[x].innerHTML;
+        }
+
+        switch (input.type) {
+            case "datetime-local":
+                input.value = new Date(row.children[x].innerHTML).toISOString().substr(0, 16);
+                break;
+            case "checkbox":
+                input.checked = row.children[x].children[0].checked;
+                break;
+            default:
+                input.value = row.children[x].innerHTML;
+        }
+    }
+}
+
+function copyFormToObject(form) {
+    let object = new Object();
     object.ID = 0;
-    var div = this.querySelector('div');
+
+    let div = form.querySelector('div');
 
     for (var i = 0; i < div.childElementCount; i++) {
         let name = div.children[i].name;
@@ -63,44 +180,6 @@ function submitHandler(event) {
         }
     }
 
-    var method = (object.ID == 0) ? 'POST' : 'PUT';
-    var link = this.dataset.link;
-
-    if (object.ID != 0) {
-        link += "/" + object.ID;
-    }
-
-    let request = new XMLHttpRequest();
-    request.open(method, link);
-    request.send(JSON.stringify(object));
-    request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-            if (request.status == 200) {
-                // TODO @ffcf mete os dados novos na tabela and fadeoff the editor
-                document.getElementById("single-form").className = "fadeOut";
-                if(method == "PUT") {
-                    var row = document.querySelector('tr[data-id="' + object.ID + '"]');
-                    for (x = 0; x < row.childElementCount - 1; x++) {
-                        let tbspace = row.children[x];
-                        let val = object[x];
-
-                        switch (tbspace.dataset.name) {
-                            case "Expires":
-                                tbspace.value = new Date(object.Expires).toISOString().substr(0, 16);
-                                break;
-                            case "Deactivated":
-                                tbspace.checked = object.Deactivated.checked;
-                                break;
-                            default:
-                                tbspace.innerHTML = object[tbspace.dataset.name];
-                        }
-                    }
-                } else {
-                    window.location.pathname = "/admin/" + window.location.pathname.split("/")[2] + "/" + object.ID;
-                }
-            } else {
-                formError("Something went wrong.", "error")
-            }
-        }
-    }
+    if (isNaN(object.ID)) object.ID = 0;
+    return object;
 }
