8c1aa8a2ed6d70600f7b1655e3d947bb1b133ede
fabioswartz <ffcf.pt.en@gmail.com>
2016-10-14 00:47:35 +0100

    Updates on mentors page

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -3,6 +3,12 @@ smoothScroll.init({
     speed: 1000,
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    if(window.location.pathname == "/mentors/") {
+        initMentorsPage();
+    }
+});
+
 window.addEventListener('scroll', function(e) {
     let nav = document.querySelector("nav");
 
@@ -12,3 +18,36 @@ window.addEventListener('scroll', function(e) {
         nav.classList.add("scroll");
     }
 });
+
+function initMentorsPage() {
+    let images = document.querySelectorAll("#mentors .container .mentor img");
+    [].forEach.call(images, function(img) {
+        img.addEventListener("click", openMentorPopup)
+    });
+}
+
+function openMentorPopup() {
+    let mentor = this.parentElement,
+        popup = document.querySelector(".mentor-popup"),
+        overlay = document.querySelector(".overlay"),
+        closeIcon = popup.querySelector(".close-icon");
+    closeIcon.addEventListener("click", closeMentorPopup);
+    popup.querySelector("img").src = mentor.querySelector("img").src;
+    popup.querySelector(".mentor-popup-name").innerHTML = mentor.querySelector(".mentor-name").innerHTML;
+    popup.querySelector(".mentor-popup-role").innerHTML = mentor.querySelector(".mentor-role").innerHTML;
+    popup.querySelector(".mentor-popup-job").innerHTML = mentor.querySelector(".mentor-job").innerHTML;
+    popup.querySelector(".mentor-popup-description").innerHTML = mentor.querySelector(".mentor-description").innerHTML;
+
+    overlay.style.display = "block";
+    popup.style.display = "block";
+    window.addEventListener("click", closeMentorPopup);
+}
+
+function closeMentorPopup(event) {
+    let overlay = document.querySelector("#mentors .overlay"),
+        popup = document.querySelector("#mentors .mentor-popup");
+    if(event.target.className == "overlay" || event.target.className == "close-icon") {
+        document.querySelector(".overlay").style.display = "none";
+        document.querySelector(".mentor-popup").style.display = "none";
+    }
+}
