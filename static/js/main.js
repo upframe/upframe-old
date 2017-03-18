5d744ba3d58b201e1b17a7b7c5f1cd436f8f539a
Henrique Dias <hacdias@gmail.com>
2017-03-18 14:02:24 +0000

    API

--- static/js/main.js
+++ static/js/main.js
@@ -1,113 +1,30 @@
-if (typeof smoothScroll != 'undefined') {
-    smoothScroll.init({
-        offset: 150,
-        speed: 1000,
-    });
-}
+'use strict';
 
-document.addEventListener("DOMContentLoaded", () => {
-    if (window.location.pathname == "/") {
-        initNewsletter();
-    }
+var apiURL = 'http://api.localhost/'
 
-    if (window.location.pathname == "/mentors") {
-        initMentorsPage();
-    }
+/* MENTORS PAGE */
 
-    if (window.location.pathname == "/apply") {
-        initApplyPage();
-    }
+let mentors = {
+    overlay: null
+};
 
-    Array.from(document.querySelectorAll(".writing-effect")).forEach(input => {
-        input.addEventListener('focus', event => {
-            event.currentTarget.parentElement.classList.remove("written");
-            event.currentTarget.parentElement.classList.add("writing");
-        });
+mentors.init = () => {
+    mentors.overlay = document.querySelector('.overlay'),
+        mentors.popup = document.querySelector('.mentor-popup');
 
-        input.addEventListener('blur', event => {
-            event.currentTarget.parentElement.classList.remove("writing");
+    mentors.overlay.addEventListener('click', mentors.close);
+    document.querySelector('.mentor-popup .close-icon').addEventListener('click', mentors.close)
+    window.addEventListener('keydown', mentors.close);
 
-            if (event.currentTarget.value != "") {
-                event.currentTarget.parentElement.classList.add("written");
-            } else {
-                event.currentTarget.parentElement.classList.remove("written");
-            }
-        });
-    });
-});
+    mentors.randomize();
 
-window.addEventListener('scroll', function(e) {
-    let nav = document.querySelector("nav");
-
-    if (window.scrollY == 0) {
-        nav.classList.remove("scroll");
-    } else {
-        nav.classList.add("scroll");
-    }
-});
-
-
-function distributePhotos(mentors) {
-    let max = mentors.length - 1,
-        s = new Set();
-    a = 0;
-    num = 0;
-
-    for (; s.size < 4;) {
-        a = Math.floor(Math.random() * (max + 1));
-        s.add(a);
-    }
-
-    for (i of document.querySelectorAll("#our-mentors div img")) {
-        num = s.keys().next().value;
-        i.src = "/img/mentors/" + mentors[num].Slug + ".jpg";
-        s.delete(num);
-    }
-}
-
-function initNewsletter() {
-    document.querySelector('#newsletter form').addEventListener("submit", function() {
-        event.preventDefault();
-        let req = new XMLHttpRequest(),
-            data = new FormData(document.querySelector("#newsletter form"));
-        req.open("POST", "/newsletter")
-        req.send(data);
-        req.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                switch (this.status) {
-                    case 500:
-                        console.log("Error: " + this.responseText);
-                    case 200:
-                        location.reload();
-                }
-            }
-        }
-    });
-}
-
-function initMentorsPage() {
-    let mentorslist = [],
-        children = document.querySelector("#mentors-container").children;
-
-    for (let i = 0; i < children.length; i++) {
-        mentorslist.push(children[i].cloneNode(true));
-    }
-
-    mentorslist.sort(function() {
-        return .5 - Math.random();
-    });
-
-    for (let i = 0; i < children.length; i++) {
-        children[i].parentNode.replaceChild(mentorslist[i], children[i]);
-    }
-
-    let images = document.querySelectorAll("#mentors-container .mentor img");
-    [].forEach.call(images, function(img) {
-        img.addEventListener("click", openMentorPopup);
+    let images = document.querySelectorAll('#mentors-container .mentor img');
+    images.forEach(img => {
+        img.addEventListener('click', mentors.open);
     });
 
     // Check if there is an hash
-    if (window.location.hash != "") {
+    if (window.location.hash != '') {
         let el = document.getElementById(window.location.hash.substring(1));
         if (el == null) return;
 
@@ -115,91 +32,205 @@ function initMentorsPage() {
     }
 }
 
-function openMentorPopup(event) {
+mentors.randomize = () => {
+    let list = [],
+        children = document.querySelector('#mentors-container').children;
+
+    for (let i = 0; i < children.length; i++) {
+        list.push(children[i].cloneNode(true));
+    }
+
+    list.sort(function() {
+        return .5 - Math.random();
+    });
+
+    for (let i = 0; i < children.length; i++) {
+        children[i].parentNode.replaceChild(list[i], children[i]);
+    }
+}
+
+mentors.open = event => {
     event.preventDefault();
 
-    let mentor = event.currentTarget.parentElement,
-        popup = document.querySelector(".mentor-popup"),
-        overlay = document.querySelector(".overlay"),
-        closeIcon = popup.querySelector(".close-icon");
-    closeIcon.addEventListener("click", closeMentorPopup);
+    let mentor = event.currentTarget.parentElement;
 
-    history.pushState("", document.title, window.location.pathname + '#' + mentor.id);
+    history.pushState('', document.title, window.location.pathname + '#' + mentor.id);
 
-    popup.querySelector("img").src = mentor.querySelector("img").src;
-    popup.querySelector(".name").innerHTML = mentor.querySelector(".name").innerHTML;
-    popup.querySelector(".role").innerHTML = mentor.querySelector(".role").innerHTML;
-    popup.querySelector(".job").innerHTML = mentor.querySelector(".job").innerHTML;
-    popup.querySelector(".description").innerHTML = mentor.querySelector(".description").innerHTML;
+    mentors.popup.querySelector('img').src = mentor.querySelector('img').src;
+    mentors.popup.querySelector('.name').innerHTML = mentor.querySelector('.name').innerHTML;
+    mentors.popup.querySelector('.role').innerHTML = mentor.querySelector('.role').innerHTML;
+    mentors.popup.querySelector('.job').innerHTML = mentor.querySelector('.job').innerHTML;
+    mentors.popup.querySelector('.description').innerHTML = mentor.querySelector('.description').innerHTML;
 
-    overlay.classList.add("active");
-    popup.classList.add("active");
-    document.querySelector("body").classList.add("no-scroll");
-    window.addEventListener("click", closeMentorPopup);
-    window.addEventListener("keydown", closeMentorPopup);
+    mentors.overlay.classList.add('active');
+    mentors.popup.classList.add('active');
+
+    document.querySelector('body').classList.add('no-scroll');
 }
 
-function closeMentorPopup(event) {
-    let overlay = document.querySelector("#mentors .overlay"),
-        popup = document.querySelector("#mentors .mentor-popup");
-    if (event.target.className == "overlay active" || event.target.className == "close-icon" || event.key == "Escape") {
-        overlay.classList.remove("active");
-        history.pushState("", document.title, window.location.pathname);
-        popup.classList.remove("active");
-        document.querySelector("body").classList.remove("no-scroll");
+mentors.close = event => {
+    if (event.target.className == 'overlay active' || event.target.className == 'close-icon' || event.key == 'Escape') {
+        mentors.overlay.classList.remove('active');
+        mentors.popup.classList.remove('active');
+
+        history.pushState('', document.title, window.location.pathname);
+        document.querySelector('body').classList.remove('no-scroll');
     }
 }
 
-function initApplyPage() {
-    let overlay = document.querySelector('.overlay'),
-        body = document.querySelector('body'),
-        tac = document.querySelector('div#terms-and-conditions');
+/* APPLY PAGE */
 
-    document.getElementById('tac-link').addEventListener('click', event => {
-        tac.classList.add('active');
-        overlay.classList.add("active");
-        body.classList.add("no-scroll");
-    });
+let apply = {
+    overlay: null,
+    tac: null
+}
 
-    let closeTermsAndConditions = function(event) {
-        event.preventDefault();
+apply.init = () => {
+    apply.overlay = document.querySelector('.overlay'),
+        apply.tac = document.querySelector('div#terms-and-conditions');
 
-        tac.classList.remove('active');
-        overlay.classList.remove("active");
-        body.classList.remove("no-scroll");
+    document.getElementById('tac-link').addEventListener('click', apply.openTac);
+    document.getElementById('tac-close').addEventListener('click', apply.closeTac);
+    document.querySelector('.overlay').addEventListener('click', apply.closeTac)
+    document.querySelector('form').addEventListener('submit', apply.submit);
+}
+
+apply.openTac = event => {
+    event.preventDefault();
+
+    apply.tac.classList.add('active');
+    apply.overlay.classList.add('active');
+    document.body.classList.add('no-scroll');
+}
+
+apply.closeTac = event => {
+    event.preventDefault();
+
+    apply.tac.classList.remove('active');
+    apply.overlay.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+}
+
+apply.submit = event => {
+    event.preventDefault();
+
+    let req = new XMLHttpRequest(),
+        form = event.currentTarget,
+        data = new FormData(form);
+
+    // TODO: JS DATA VALIDITY CHECK
+
+    req.open('POST', apiURL + '/apply');
+    req.send(data);
+    req.onload = function() {
+        switch (this.status) {
+            case 200:
+                form.classList.add('success');
+                form.querySelector('p').style.display = 'block';
+                form.querySelector('.btn').style.display = 'none';
+                break;
+            default:
+                form.classList.add('error');
+                form.querySelector('.btn').value = 'Something went wrong';
+        }
     }
 
-    document.getElementById('tac-close').addEventListener('click', closeTermsAndConditions);
-    document.querySelector('.overlay').addEventListener('click', closeTermsAndConditions)
+    return;
+}
 
-    document.querySelector('form').addEventListener("submit", function(event) {
-        event.preventDefault();
+/* HOME PAGE */
+let home = {};
 
-        let req = new XMLHttpRequest(),
-            form = document.querySelector("form"),
-            data = new FormData(form);
+home.randomizePhotos = mentors => {
+    let max = mentors.length - 1,
+        s = new Set(),
+        a = 0,
+        num = 0;
 
-        // TODO: JS DATA VALIDITY CHECK
+    for (; s.size < 4;) {
+        a = Math.floor(Math.random() * (max + 1));
+        s.add(a);
+    }
 
-        req.open("POST", window.location);
-        req.send(data);
-        req.onreadystatechange = function() {
-            if (this.readyState == 4) {
-                switch (this.status) {
-                    case 200:
-                        form.classList.add('success');
-                        form.querySelector('p').style.display = "block";
-                        form.querySelector('.btn').style.display = "none";
-                        break;
-                    default:
-                        form.classList.add('error');
-                        form.querySelector(".btn").value = "Something went wrong";
-                }
+    for (let i of document.querySelectorAll('#our-mentors div img')) {
+        num = s.keys().next().value;
+        i.src = '/img/mentors/' + mentors[num].Slug + '.jpg';
+        s.delete(num);
+    }
+}
 
-                return;
-            }
+home.newsletter = event => {
+    event.preventDefault();
+
+    let req = new XMLHttpRequest(),
+        data = new FormData(event.currentTarget);
+
+    req.open('POST', apiURL + '/newsletter')
+    req.send(data);
+    req.onerror = () => {
+        console.log('Error: ' + req.responseText);
+    }
+
+    req.onload = function() {
+        if (req.status == 200) {
+            location.reload();
+            return;
         }
 
-        return;
-    });
+        console.log('Error: ' + req.responseText);
+    }
 }
+
+/* GENERAL AND BASE FUNCTIONS */
+
+function scrollEvent(event) {
+    let nav = document.querySelector('nav');
+
+    if (window.scrollY == 0) {
+        nav.classList.remove('scroll');
+    } else {
+        nav.classList.add('scroll');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (typeof smoothScroll != 'undefined') {
+        smoothScroll.init({
+            offset: 150,
+            speed: 1000,
+        });
+    }
+
+    window.addEventListener('scroll', scrollEvent);
+
+    if (window.location.pathname == '/') {
+        document.querySelector('#newsletter form').addEventListener('submit', home.newsletter);
+    }
+
+    if (window.location.pathname == '/mentors') {
+        mentors.init();
+    }
+
+    if (window.location.pathname == '/apply') {
+        apply.init();
+    }
+
+    Array.from(document.querySelectorAll('.writing-effect')).forEach(input => {
+        let parent = input.parentElement;
+
+        input.addEventListener('focus', event => {
+            parent.classList.remove('written');
+            parent.classList.add('writing');
+        });
+
+        input.addEventListener('blur', event => {
+            parent.classList.remove('writing');
+
+            if (event.currentTarget.value != '') {
+                parent.classList.add('written');
+            } else {
+                parent.classList.remove('written');
+            }
+        });
+    });
+});
