5f1a8f4de2b9b296021047de64189763c1dead67
Fábio Ferreira <ffcfpten@gmail.com>
2018-02-24 02:27:14 +0000

    fix: reading progress bar bug fixing

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
+function getDocHeight() {
+  let D = document;
+  return Math.max(
+    D.body.scrollHeight, D.documentElement.scrollHeight,
+    D.body.offsetHeight, D.documentElement.offsetHeight,
+    D.body.clientHeight, D.documentElement.clientHeight
+  )
+}
+
+function scrollPerc() {
+  let windowHeight = window.innerHeight || (document.documentElement || document.body).clientHeight,
+      docHeight = getDocHeight(),
+      scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  return Math.floor((scrollTop / (docHeight - windowHeight)) * 100); 
+}
+
 document.addEventListener("scroll", ev => {
-  document.querySelector("span#read-progress").style.width = String((window.pageYOffset / document.body.offsetHeight) * 100 + "%");
+  document.querySelector("span#read-progress").style.width = String(scrollPerc() + "%");
 });
