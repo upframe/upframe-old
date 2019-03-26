2299380a3823027c6d0047c6998f31dd7d12315f
Ulisses Ferreira <fc49430@alunos.fc.ul.pt>
2019-03-26 01:41:45 +0000

    [wip] feat: FAQ area

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 function init() {
+  document.getElementById('1').addEventListener('click', function () {
+    document.getElementById('answer-1').classList.toggle('none')
+    document.getElementById('signal-1').innerHTML = document.getElementById('signal-1').innerHTML === '+' ? '-' : '+'
+  });
+  document.getElementById('2').addEventListener('click', function() {
+    document.getElementById('answer-2').classList.toggle('none')
+    document.getElementById('signal-2').innerHTML = document.getElementById('signal-2').innerHTML === '+' ? '-' : '+'
+  });
+  document.getElementById('3').addEventListener('click', function() {
+    document.getElementById('answer-3').classList.toggle('none')
+    document.getElementById('signal-3').innerHTML = document.getElementById('signal-3').innerHTML === '+' ? '-' : '+'
+  });
 
+}
+
+function slideDownFaq() {
+  document.querySelector('#faq').scrollIntoView()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
