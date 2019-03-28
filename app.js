aef7645bbb0ceb2b2a4ff6b9b213c9aa051e496c
Fábio Ferreira <ffcfpten@gmail.com>
2019-03-28 11:45:16 +0000

    feat: basic hamburger

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-function init() {
+function addHamburger() {
+  let hamburgerMenu = document.querySelector('.hamburger-menu')
 
+  if (hamburgerMenu) {
+    // add .active class on click
+    hamburgerMenu.addEventListener('click', (e) => {
+      let icon = document.querySelector('.hamburger')
+      
+      if (icon.classList.contains('active')) {
+        document.querySelector('.hamburger-menu').classList.remove('open')
+        icon.classList.remove('active')
+      } else {
+        document.querySelector('.hamburger-menu').classList.add('open')
+        icon.classList.add('active')
+      }
+    })
+
+    document.body.addEventListener('click', (e) => {
+      let target = e.target
+    })
+  }
+}
+
+
+function init() {
+  addHamburger()
 }
 
 document.addEventListener('DOMContentLoaded', () => {
